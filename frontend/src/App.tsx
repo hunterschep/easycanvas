b963217673f5960e7473839047d34f8f9bb43053
Hunter S <67876717+hunterschep@users.noreply.github.com>
2024-12-26 21:32:38 -0800

    little ui fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <Routes>
         <Route 
           path="/login" 
-          element={user ? <Navigate to="/" /> : <Login />} 
+          element={user ? <Navigate to="/setup" /> : <Login />} 
         />
         <Route 
           path="/setup" 
